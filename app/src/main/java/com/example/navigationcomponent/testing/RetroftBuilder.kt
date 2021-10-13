c96001401bbb3c8666e2eba740f06ab64b357209
Priyadharshini <priyadharshini.c@indiumsoft.com>
2021-10-13 14:54:11 +0530

    paging library

--- app/src/main/java/com/example/navigationcomponent/testing/RetroftBuilder.kt
+++ app/src/main/java/com/example/navigationcomponent/testing/RetroftBuilder.kt
@@ -1,6 +1,6 @@
  package com.example.navigationcomponent.testing
 
-import com.manager.retrofitesting.model.api.ApiInterface
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
